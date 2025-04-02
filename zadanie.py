b0892d31f2495de0d76ca33e21c4a7484c8909bb
xwerx3123 <wrx93204@student.wroclaw.merito.pl>
2025-04-02 12:45:51 +0200

    zadanie

--- zadanie.py
+++ zadanie.py
@@ -31,3 +31,67 @@ def our_snake(snake_blok, snake_list):
         for x in snake_list:
             pygame.draw.rect(screen, black, (x[0], x[1], snake_blok, snake_blok))
 
+def wiadomosc(msg,kolor, x, y):
+    mesg = styl_czcionki.render(msg, True, kolor)
+    screen.blit (mesg, (x, y))
+
+def punktacja(score):
+    styl_czcionki = score_font.render(f"Score: {score}", True, red)
+    screen.blit(styl_czcionki, [wysokosc, -240, 20])
+
+
+def gameloop():
+    game_over = False
+    game_close = False
+    game_pause = False
+    game_start = False
+
+
+    x1 = szerokosc /2
+    y1 = wysokosc /2
+
+    x1_change = 0
+    y1_change = 0
+
+    snake_list = []
+    length_of_snake = 1
+
+    foodx = round(random.randrange(0, szerokosc -  snake_blok) / 10.0) * 10
+    foody = round(random.randrange(0, wysokosc - snake_blok) / 10.0) * 10
+
+
+    score = 0
+
+    while not game_start:
+
+        wiadomosc("nacisnij S aby rozpoaczac, pod klawiszem P masz pauze ", red, szerokosc / 6, wysokosc / 3,)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    game_start = True
+
+    while not game_over:
+        while game_close:
+            screen.fill(black)
+            wiadomosc("Przegrałeś! Naciśnij G- aby wyjsc albo O- aby zagrac ponownie ", red, szerokosc / 6, wysokosc / 3)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_g:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_o:
+                        game_close = False
+                        x1, y1 = szerokosc //2, wysokosc //2
+                        x1_change, y1_change = 0, 0
+                        snake_list = []
+                        score = 0
+                        foodx = round (random.randrange (0, szerokosc - snake_blok) / 10.0) * 10
+                        foody = round (random.randrange (0, wysokosc - snake_blok) / 10.0) * 10
+
+                        gameloop()
