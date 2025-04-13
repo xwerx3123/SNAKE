2770c42fd28a007bb1f7a0d55bad4a8c7403f166
adamusek-007 <adam.kuczabinski@gmail.com>
2025-04-13 13:03:53 +0200

    Autoformat code

--- zadanie.py
+++ zadanie.py
@@ -3,16 +3,16 @@ import random
 
 pygame.init()
 
-white = (255,255,255)
-black = (0,0,0)
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
 
 szerokosc = 1200
 wysokosc = 600
 
-screen = pygame.display.set_mode((szerokosc,wysokosc))
+screen = pygame.display.set_mode((szerokosc, wysokosc))
 
 
 snake_blok = 10
@@ -20,19 +20,22 @@ snake_speed = 15
 
 clock = pygame.time.Clock()
 
-styl_czcionki = pygame.font.SysFont('comicsans', 30)
+styl_czcionki = pygame.font.SysFont("comicsans", 30)
+
 
 def snake(snake_blok, snake_list):
-        for x in snake_list:
-            pygame.draw.rect(screen, green, (x[0], x[1], snake_blok, snake_blok))
+    for x in snake_list:
+        pygame.draw.rect(screen, green, (x[0], x[1], snake_blok, snake_blok))
 
-def wiadomosc(msg,kolor, x, y):
+
+def wiadomosc(msg, kolor, x, y):
     mesg = styl_czcionki.render(msg, True, kolor)
-    screen.blit (mesg, (x, y))
+    screen.blit(mesg, (x, y))
+
 
 def punktacja(score, screen):
-    value =  styl_czcionki.render(f"Punktacja:  {score}", True, red)
-    screen.blit(value, (szerokosc -250, 10))
+    value = styl_czcionki.render(f"Punktacja:  {score}", True, red)
+    screen.blit(value, (szerokosc - 250, 10))
 
 
 def gameloop():
@@ -41,9 +44,8 @@ def gameloop():
     game_pause = False
     game_start = False
 
-
-    x1 = szerokosc /2
-    y1 = wysokosc /2
+    x1 = szerokosc / 2
+    y1 = wysokosc / 2
 
     x1_change = 0
     y1_change = 0
@@ -51,15 +53,17 @@ def gameloop():
     snake_list = []
     dlugosc_snake = 1
 
-    foodx = round(random.randrange(0, szerokosc -  snake_blok) / 10.0) * 10
+    foodx = round(random.randrange(0, szerokosc - snake_blok) / 10.0) * 10
     foody = round(random.randrange(0, wysokosc - snake_blok) / 10.0) * 10
-
-
-
 
     while not game_start:
 
-        wiadomosc("wcisnij S aby rozpoaczac, wcisnij P aby zatrzymac ", red, szerokosc / 5, wysokosc / 4)
+        wiadomosc(
+            "wcisnij S aby rozpoaczac, wcisnij P aby zatrzymac",
+            red,
+            szerokosc / 5,
+            wysokosc / 4,
+        )
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -72,7 +76,12 @@ def gameloop():
     while not game_over:
         while game_close:
             screen.fill(black)
-            wiadomosc("Przegrałeś! Wcisnij G- aby wyjsc albo T- aby zagrac ponownie ", red, szerokosc / 6, wysokosc / 3)
+            wiadomosc(
+                "Przegrałeś! Wcisnij G- aby wyjsc albo T- aby zagrac ponownie ",
+                red,
+                szerokosc / 6,
+                wysokosc / 3,
+            )
             pygame.display.update()
 
             for event in pygame.event.get():
@@ -82,39 +91,44 @@ def gameloop():
                         game_close = False
                     if event.key == pygame.K_t:
                         game_close = False
-                        x1, y1 = szerokosc //2, wysokosc //2
+                        x1, y1 = szerokosc // 2, wysokosc // 2
                         x1_change, y1_change = 0, 0
                         snake_list = []
-                        foodx = round (random.randrange (0, szerokosc - snake_blok) / 10.0) * 10
-                        foody = round (random.randrange (0, wysokosc - snake_blok) / 10.0) * 10
+                        foodx = (
+                            round(random.randrange(0, szerokosc - snake_blok) / 10.0)
+                            * 10
+                        )
+                        foody = (
+                            round(random.randrange(0, wysokosc - snake_blok) / 10.0)
+                            * 10
+                        )
 
                         gameloop()
 
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    game_over = True
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        x1_change = -snake_blok
-                        y1_change = 0
-                    elif event.key == pygame.K_RIGHT:
-                        x1_change = snake_blok
-                        y1_change = 0
-                    elif event.key == pygame.K_UP:
-                        y1_change = -snake_blok
-                        x1_change = 0
-                    elif event.key == pygame.K_DOWN:
-                        y1_change = snake_blok
-                        x1_change = 0
-                    elif event.key == pygame.K_p:
-                        game_pause = not game_pause
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_blok
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_blok
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_blok
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_blok
+                    x1_change = 0
+                elif event.key == pygame.K_p:
+                    game_pause = not game_pause
 
         if game_pause:
             pygame.display.update()
             continue
 
-
-        if x1 > szerokosc or x1 < 0 or  y1 > wysokosc or y1 < 0:
+        if x1 > szerokosc or x1 < 0 or y1 > wysokosc or y1 < 0:
             game_close = True
 
         x1 += x1_change
@@ -136,25 +150,23 @@ def gameloop():
         snake(snake_blok, snake_list)
         pygame.display.update()
 
-        punktacja (dlugosc_snake -1, screen)
-        pygame.display.update ()
-
+        punktacja(dlugosc_snake - 1, screen)
+        pygame.display.update()
 
         if x1 == foodx and y1 == foody:
-            foodx =  round(random.randrange(0, szerokosc - snake_blok) / 10.0) * 10
+            foodx = round(random.randrange(0, szerokosc - snake_blok) / 10.0) * 10
             foody = round(random.randrange(0, wysokosc - snake_blok) / 10.0) * 10
             dlugosc_snake += 1
 
-
         if dlugosc_snake >= 50:
-            wiadomosc("Gratulacje wygrałeś", red , szerokosc / 6, wysokosc / 3)
+            wiadomosc("Gratulacje wygrałeś", red, szerokosc / 6, wysokosc / 3)
             pygame.display.update()
             game_close = True
-
 
         clock.tick(snake_speed)
 
     pygame.quit()
     quit()
+
 
 gameloop()
